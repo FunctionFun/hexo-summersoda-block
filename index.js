ebcaac2be3e255b55648b89cd8525dbb9afed922
FunctionFun <functionfun@qq.com>
2022-07-18 19:05:05 +0800

    增加一个 demo 标签

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ hexo.extend.tag.register("music", require("./lib/music")(hexo));
 
 hexo.extend.tag.register("video", require("./lib/video")(hexo));
 
+hexo.extend.tag.register("demo", require("./lib/demo")(hexo), { ends: true });
+
 hexo.extend.tag.register("mk", require("./lib/mk")(hexo), { ends: true });
 
 hexo.extend.filter.register('after_render:html', require("./lib/loadLib")(hexo));
